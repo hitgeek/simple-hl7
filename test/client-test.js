4b2363ef27fe223cb1889a4be60b81be87e8b0da
Jonathan Minson <jonathan.m.minson@gmail.com>
2023-11-22 09:01:48 -0600

    Fix port

--- test/client-test.js
+++ test/client-test.js
@@ -4,7 +4,7 @@ var Parser = require('../lib/hl7/parser');
 // do not remove this line... it is used to test the server
 require('../lib/index');
 
-var client = hl7.Server.createTcpClient('localhost', 7777);
+var client = hl7.Server.createTcpClient('localhost', 7700);
 
 //create a message
 var msgString =
